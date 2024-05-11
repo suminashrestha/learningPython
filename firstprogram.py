486870046b41229ad49183f3eac58fd099b1e3bb
suminashrestha <suminastha77@gmail.com>
2024-05-11 20:09:56 +0545

    Day1

---
+++ firstprogram.py
@@ -0,0 +1,18 @@
+name = "Sumina"
+age = 22
+price = 24.55
+isOld = False
+a= None
+
+# Data Types
+print(age)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(isOld))
+print(type(a)) 
+
+n1 = 45
+n2 = 45
+sum = n1+ n2
+print(sum)
